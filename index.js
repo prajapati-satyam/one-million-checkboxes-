4bd25ee6334ecf80aa2f11235bbe49261d73c63c
prajapati-satyam <satyamprajapati1979@gmail.com>
2026-04-19 11:50:43 +0530

    --change port from 3002 to 3000

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
 const {Server} = require('socket.io');
 const http = require('http');
 const Redis = require('ioredis');
